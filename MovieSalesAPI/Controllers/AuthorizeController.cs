355fc5d1c36f4ffbb5bf62918ce10c3ff4070a8a
Kyle Denney <kndenney@gmail.com>
2018-07-01 22:21:41 -0400

    Added TODO in appsettings. Added Create Token API POST method updates.

--- MovieSalesAPI/Controllers/AuthorizeController.cs
+++ MovieSalesAPI/Controllers/AuthorizeController.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MovieSalesAPILogic;
 
 namespace MovieSalesAPI.Controllers
 {
@@ -15,48 +21,52 @@ namespace MovieSalesAPI.Controllers
     public class AuthorizeController : ControllerBase
     {
         //https://jonhilton.net/2017/10/11/secure-your-asp.net-core-2.0-api-part-1---issuing-a-jwt/
-
-        // GET: api/Authorize
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Authorize/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //https://auth0.com/blog/securing-asp-dot-net-core-2-applications-with-jwts/
 
         // POST: api/Authorize
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="value"></param>
+        /// <param name="request">Token Request posted in the form of Username and Password</param>
         [Route("create/token")]
+        [AllowAnonymous]
         [HttpPost]
-        public void CreateToken([FromBody] string value)
+        public string CreateToken([FromBody] TokenRequest request)
         {
+            //The username and password would be pulled either from a secure server
+            //or database in our class library codebase or some other DAL
+            if (request.Username == "Jon" && request.Password == "Again, not for production use, DEMO ONLY!")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username)
+                };
 
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-        }
+                // _config["Jwt:Issuer"]
+                //We should create a QA URL and Prod URL in the appsettings.json
+                //then use that here
+                //in the 'issuer' and 'audience' portion
+                //whereby the issueing server is whatever we need it to be
+                //should it be diference than the audience server (audience being
+                //being the Angular app or front end whatever domain name
+                //and the issuing server could be a different domain name altogether
+                //like token.domain.com
+                //and audienc would just be domain.com etc.
 
-        // PUT: api/Authorize/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+                var token = new JwtSecurityToken(
+                    issuer: "yourdomain.com",
+                    audience: "yourdomain.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+
+            return "";
         }
     }
 }
---
+++ MovieSalesAPILogic/Authorization/TokenRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MovieSalesAPILogic
+{
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
